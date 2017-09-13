b29af0098b467249018c8fc20aafbb86884006f9
Henry <henry.m.zhang@connect.polyu.hk>
2017-09-13 15:24:21 +0800

    完善基本目录结构，需要junit

---
+++ src/hk/edu/polyu/comp/comp2021/monopoly/Game.java
@@ -0,0 +1,7 @@
+package hk.edu.polyu.comp.comp2021.monopoly;
+
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ test/hk/edu/polyu/comp/comp2021/test/GameTest.java
@@ -0,0 +1,20 @@
+package hk.edu.polyu.comp.comp2021.test;
+
+import hk.edu.polyu.comp.comp2021.monopoly.Game;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class GameTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testMain(){
+        Game.main(null);
+    }
+
+}
