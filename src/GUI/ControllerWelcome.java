801feee4e996f703975fee0d6fb8c2c9bd0c8458
TCtower <wjrdjowo@gmail.com>
2017-11-24 18:14:55 +0800

    Add Controller

--- src/GUI/ControllerWelcome.java
+++ src/GUI/ControllerWelcome.java
@@ -1,4 +1,36 @@
 package GUI;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
 public class ControllerWelcome {
+    @FXML
+    private Button NewGame;
+    @FXML
+    private Button LoadGame;
+    @FXML
+    private Button Quit;
+
+    @FXML
+    public void HandleNewGame(ActionEvent event){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("FormNewGame.fxml"));
+            Main.setStage(root);
+        } catch (Exception e){
+            System.out.print(e);
+        }
+    }
+
+    @FXML
+    public void HandleLoadGame(ActionEvent event){
+
+    }
+
+    @FXML
+    public void HandleQuit(ActionEvent event){
+        System.exit(0);
+    }
 }
