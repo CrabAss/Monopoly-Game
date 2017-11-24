ff3131d1acd0d13a94f07a2b3a4dd5a0d4e30871
TCtower <wjrdjowo@gmail.com>
2017-11-25 00:34:58 +0800

    Add test for player move

--- src/GUI/ControllerGame.java
+++ src/GUI/ControllerGame.java
@@ -1,4 +1,21 @@
 package GUI;
 
+import com.sun.org.apache.regexp.internal.RE;
+import javafx.fxml.FXML;
+import javafx.scene.shape.*;
+import javafx.scene.layout.GridPane;
+import javafx.event.ActionEvent;
+
 public class ControllerGame {
+    @FXML
+    private GridPane Central;
+    @FXML
+    private Rectangle RectangleRed;
+
+    @FXML
+    public void HandleAction(ActionEvent event){
+        Rectangle t;
+        t = RectangleRed;
+        Central.add(t, 0, 0);
+    }
 }
