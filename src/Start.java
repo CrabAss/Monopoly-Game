d8508c922a9dabaa40820362f2fdb12982732f87
TCtower <wjrdjowo@gmail.com>
2017-11-24 10:45:22 +0800

    version_1.0

---
+++ src/Start.java
@@ -0,0 +1,37 @@
+import Game.Game;
+import java.util.*;
+
+public class Start {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        int command = 0;
+        System.out.println("This game is fictional.");
+        while (true) {
+            System.out.println("Monopoly");
+            System.out.println("1. New game");
+            System.out.println("2. Load game");
+            System.out.println("3. Quit");
+            try {
+                if (inp.hasNextInt()) command = inp.nextInt();
+                else{
+                    System.out.print("Wrong");
+                    continue;
+                }
+                if (command == 3) {
+                    System.out.println("Thanks for playing!");
+                    break;
+                } else if (command == 1) {//Start a new game
+                    Game game = new Game();
+                    game.newGame();
+                } else if (command == 2) {
+                    Game game = new Game();
+                    game.loadGame();
+                } else System.out.println("Wrong input! Please input again");
+            }catch (InputMismatchException e){
+                System.out.println("Wrong input! Please input again");
+                break;
+            }
+        }
+    }
+}
